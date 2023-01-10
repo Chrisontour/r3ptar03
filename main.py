1681866dad1c2828b45a3c3d49d28f8a25415a85
Chrisontour <122272439+Chrisontour@users.noreply.github.com>
2023-01-10 14:30:43 +0100

    Update main.py
    change Motor names de->eng
    reorganise attack sequence

--- main.py
+++ main.py
@@ -7,29 +7,28 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
 
 # Create your objects here.
 ev3 = EV3Brick()
-KopfMotor = Motor(Port.D)
-SlitherMotor = Motor(Port.A)
-SchubMotor = Motor(Port.B)
+headMotor = Motor(Port.D)
+slitherMotor = Motor(Port.A)
+pushMotor = Motor(Port.B)
 obstacleSensor = InfraredSensor(Port.S4)
 
-
 # Write your program here.
-
-KopfMotor.run_time(-300,1000)
+headMotor.run_time(-300,1000)
 while True:
     if obstacleSensor.distance() < 30:
-        ev3.light.on(Color.RED)
         ev3.speaker.play_file(SoundFile.SNAKE_HISS)
-        KopfMotor.run_time(1000,800,Stop.COAST)
-        KopfMotor.run_time(-1000,800,Stop.COAST)
+        headMotor.run_time(1000,800,Stop.COAST)
+        ev3.light.on(Color.RED)
+        headMotor.run_time(-1000,800,Stop.COAST,wait=True)
         ev3.light.off()
         wait(1)
+    # else
     ev3.light.on(Color.YELLOW)
     wait(1000)
+    # drive backwards
+    # change direction
+    # slither around
