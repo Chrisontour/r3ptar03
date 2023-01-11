b6a1f10b48fa18da400b4dd258b18cd11c09332b
Chrisontour <122272439+Chrisontour@users.noreply.github.com>
2023-01-11 16:50:15 +0100

    try to integrate threads and procedures

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env pybricks-micropython
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -7,7 +9,7 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
+import _thread
 
 # Create your objects here.
 ev3 = EV3Brick()
@@ -17,18 +19,45 @@ pushMotor = Motor(Port.B)
 obstacleSensor = InfraredSensor(Port.S4)
 
 # Write your program here.
-headMotor.run_time(-300,1000)
-while True:
-    if obstacleSensor.distance() < 30:
-        ev3.speaker.play_file(SoundFile.SNAKE_HISS)
-        headMotor.run_time(1000,800,Stop.COAST)
-        ev3.light.on(Color.RED)
-        headMotor.run_time(-1000,800,Stop.COAST,wait=True)
-        ev3.light.off()
-        wait(1)
-    # else
-    ev3.light.on(Color.YELLOW)
+def blink(colour):
+  while True:
+    ev3.light.on(Color.colour)
+    wait(200)
+    ev3.light.off()
+    wait(200)
+
+def attack():
+    ev3.speaker.play_file(SoundFile.SNAKE_HISS)
+    headMotor.run_time(1000,800,Stop.COAST, wait=False)
+    #ev3.light.on(Color.RED)
+    blinke(RED)
+    headMotor.run_time(-1000,800,Stop.COAST,wait=False)
     wait(1000)
-    # drive backwards
-    # change direction
-    # slither around
+    hide()
+    wait(1000)
+    ev3.light.off()
+
+def slither():
+    #ev3.light.on(Color.YELLOW)
+    blink(GREEN)
+        # slither around
+    pushMotor.run_time(1000,1000)
+    ev3.light.off()
+
+def hide():
+    pushMotor.run_time(-800,2000, wait=False)
+
+def main_procedure():
+    while True:
+    try:
+        if obstacleSensor.distance() < 30:
+            attack()
+        # else
+        slither()
+    except:
+        print "Error!"
+
+# start:
+headMotor.run_time(-300,1000)
+_thread.start_new_thread(blink())
+_thread.start_new_thread(main_procedure())
